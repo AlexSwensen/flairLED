0b2cccc0c4eb44ed700656dba8ead7d6ab711e43
Alexander Swensen <alex.swensen@gmail.com>
2017-07-20 18:22:16 -0500

    Reorganize some type files

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { SerialPort } from 'serialport';
 if (environment.production) {
   enableProdMode();
 }

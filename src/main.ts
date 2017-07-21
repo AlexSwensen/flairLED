3ffc8b20f47582ca25df236b711b368ef4e2a993
Alexander Swensen <alex.swensen@gmail.com>
2017-07-20 20:06:36 -0500

    Put it where it belongs (for now)

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-
-declare var SerialPort: any;
-
-SerialPort.list(function(err, ports) {
-  ports.forEach(function(port) {
-
-  });
-
-});
-
 if (environment.production) {
   enableProdMode();
 }
